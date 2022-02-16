ec4b67a6bd65b2b0870ffd1c9c7efa125e6b5a3b
unioan <v.yushkov@gmail.com>
2022-02-16 16:57:29 +0300

    Controller impeded in header scrolls views successfully

--- DodoLayout/CollectionViewLayoutManager.swift
+++ DodoLayout/CollectionViewLayoutManager.swift
@@ -12,13 +12,13 @@ class CollectionViewLayoutManager {
     static func createLayout() -> UICollectionViewLayout {
         let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnviroment -> NSCollectionLayoutSection? in
             
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.02), heightDimension: .fractionalHeight(1.0))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            let itemSpacing: CGFloat = 10 // items отступят со всех краев 5 поинтов
-            item.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15)
+            let itemSpacing: CGFloat = 2 // items отступят со всех краев 5 поинтов
+            item.contentInsets = NSDirectionalEdgeInsets(top: itemSpacing, leading: itemSpacing, bottom: itemSpacing, trailing: itemSpacing)
             
             // GROUP
-            let categoryGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.1), heightDimension: .fractionalHeight(1.0))
+            let categoryGroupSize = NSCollectionLayoutSize(widthDimension: .absolute(600), heightDimension: .fractionalHeight(1.0))
             let categoryGroup = NSCollectionLayoutGroup.horizontal(layoutSize: categoryGroupSize, subitem: item, count: 1)
             
             // SECTION
